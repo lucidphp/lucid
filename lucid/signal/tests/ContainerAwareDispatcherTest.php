78ead8e533494b1af86ac9b563bbce2e606e9e26
iwyg <mail@thomas-appel.com>
2016-04-23 15:37:32 +0200

    make it actually work

--- lucid/signal/tests/ContainerAwareDispatcherTest.php
+++ lucid/signal/tests/ContainerAwareDispatcherTest.php
@@ -12,6 +12,7 @@
 namespace Lucid\Signal\Tests;
 
 use Lucid\Signal\ContainerAwareDispatcher;
+use Lucid\Signal\HandlerInterface;
 
 /**
  * @class ContainerAwareDispatcherTest
@@ -36,11 +37,17 @@ class ContainerAwareDispatcherTest extends \PHPUnit_Framework_TestCase
     {
         $container = $this->mockContainer();
         $container->method('has')->with('myservice')->willReturn(true);
+        $service = $this->getMockbuilder('MyService')
+            ->setMethods(['handleEvent'])
+            ->disableOriginalConstructor()->getMock();
+        $service->expects($this->once())->method('handleEvent');
+        $container->method('get')->with('myservice')->willReturn($service);
 
         $dispatcher = new ContainerAwareDispatcher($container);
         $dispatcher->addHandler('myevent', 'myservice@handleEvent');
 
         $this->assertTrue($dispatcher->hasEvent('myevent'));
+        $dispatcher->dispatch('myevent');
     }
 
     /** @test */
@@ -48,6 +55,8 @@ class ContainerAwareDispatcherTest extends \PHPUnit_Framework_TestCase
     {
         $container = $this->mockContainer();
         $container->method('has')->with('myservice')->willReturn(true);
+        $container->method('get')->with('myservice')->willReturn(function () {
+        });
 
         $dispatcher = new ContainerAwareDispatcher($container);
         $dispatcher->addHandler('myevent', 'myservice');
@@ -62,6 +71,7 @@ class ContainerAwareDispatcherTest extends \PHPUnit_Framework_TestCase
         $container->method('has')->with('myservice')->willReturn(false);
 
         $dispatcher = new ContainerAwareDispatcher($container);
+
         try {
             $dispatcher->addHandler('myevent', 'myservice@handleMethod');
         } catch (\InvalidArgumentException $e) {
@@ -71,6 +81,22 @@ class ContainerAwareDispatcherTest extends \PHPUnit_Framework_TestCase
         }
 
         $this->fail();
+    }
+
+    /** @test */
+    public function itShouldResolveServiceHandlers()
+    {
+        $container = $this->mockContainer();
+        $container->method('has')->with('myservice')->willReturn(true);
+        $service = $this->getMockbuilder(HandlerInterface::class)
+            ->disableOriginalConstructor()->getMock();
+        $service->expects($this->once())->method('handleEvent');
+
+        $container->method('get')->with('myservice')->willReturn($service);
+
+        $dispatcher = new ContainerAwareDispatcher($container);
+        $dispatcher->addHandler('myevent', 'myservice');
+        $dispatcher->dispatch('myevent');
     }
 
     private function mockContainer()
